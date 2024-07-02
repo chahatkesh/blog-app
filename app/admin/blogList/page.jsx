e64a80cbd575d183ac861ba30b4be51a1c35b237
CK5115 <ckesharwani4@gmail.com>
2024-07-03 00:45:41 +0530

    Delete functionality

--- app/admin/blogList/page.jsx
+++ app/admin/blogList/page.jsx
@@ -1,12 +1,70 @@
 "use client";
-import React from "react";
+import BlogTableItem from "@/Components/AdminComponents/BlogTableItem";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
-const page = () => {
+const Page = () => {
+  const [blogs, setBlogs] = useState([]);
+  const fetchBlogs = async () => {
+    const response = await axios.get("/api/blog");
+    setBlogs(response.data.blogs);
+  };
+
+  const deleteBlog = async (mongoId) => {
+    const response = await axios.delete("/api/blog", {
+      params: { id: mongoId },
+    });
+    toast.success(response.data.msg);
+    fetchBlogs();
+  };
+
+  useEffect(() => {
+    fetchBlogs();
+  }, []);
+
   return (
     <>
-      <div></div>
+      <div className="flex-1 pt-5 px-5 sm:pt-12 sm:pl-16">
+        <h1>All Blogs</h1>
+        <div className="relative h-[80vh] max-w-[850px] overflow-x-auto mt-4 border border-gray-400 scrollbar-hide">
+          <table className="w-full text-sm text-gray-500">
+            <thead className="text-sm text-gray-700 text-left uppercase bg-gray-50">
+              <tr>
+                <th className="hidden sm:block px-6 py-3" scope="col">
+                  Author name
+                </th>
+                <th className="px-6 py-3" scope="col">
+                  Blog title
+                </th>
+                <th className="px-6 py-3" scope="col">
+                  Blog Date
+                </th>
+                <th className="px-6 py-3" scope="col">
+                  Action
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {blogs.map((item, index) => {
+                return (
+                  <BlogTableItem
+                    key={index}
+                    mongoId={item._id}
+                    title={item.title}
+                    author={item.author}
+                    author_img={item.author_img}
+                    date={item.date}
+                    deleteBlog={deleteBlog}
+                  />
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </>
   );
 };
 
-export default page;
+export default Page;
